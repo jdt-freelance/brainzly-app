55038194f29dfbd15de3ebf9e2cbea4adebf0e89
jdt-freelance <79086503+jdt-freelance@users.noreply.github.com>
2022-12-05 10:30:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# brainzly-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/brainzly-2)
---
+++ src/components/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <>
+      <h1> Brainzly </h1>
+      <div className="wrapper"></div>
+    </>
+  );
+}
